be301325fa9f19eedb5d01b55d3888e938581e23
”junnosuke-he” <goukagu@gmail.com>
2020-10-07 04:24:43 +0000

    kadai10_4

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Profile create</title>
+    </head>
+    <body>
+        <h1>プロフィール作成画面</h1>
+    </body>
+</html>
