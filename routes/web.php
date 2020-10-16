95bfab850136f6b147ec7879ccb3e3b7f39e42b5
”junnosuke-he” <goukagu@gmail.com>
2020-10-16 10:20:15 +0000

    kadai12_2

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
  Route::group(["prefix" => "admin"], function(){
         Route::get("news/create", "Admin\NewsController@add")->middleware('auth');
         Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+        Route::get('news', 'Admin\NewsController@index')->middleware('auth');
         //4.【応用】
         Route::get("profile/create","Admin\ProfileController@add")->middleware('auth');
         Route::get("profile/edit","Admin\ProfileController@edit")->middleware('auth');
