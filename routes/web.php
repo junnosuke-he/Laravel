0135a352742505a11fa70399ac96ac24f1057a6a
”junnosuke-he” <goukagu@gmail.com>
2020-10-19 09:13:36 +0000

    kadai13

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ Route::get('/', function () {
         Route::get("news/create", "Admin\NewsController@add")->middleware('auth');
         Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
         Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+        Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+        Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+        Route::get('news/detele', 'Admin\NewsController@delete')->middleware('auth');
         //4.【応用】
         Route::get("profile/create","Admin\ProfileController@add")->middleware('auth');
         Route::get("profile/edit","Admin\ProfileController@edit")->middleware('auth');
