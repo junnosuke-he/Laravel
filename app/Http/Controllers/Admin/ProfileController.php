582bd6ba1aa67e565511c9c8a9314fb4c671f855
”junnosuke-he” <goukagu@gmail.com>
2020-10-20 22:28:05 +0000

    sitsumon

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -20,27 +20,38 @@ class ProfileController extends Controller
         $profile = new Profile;
         $form = $request->all();
         
-        if (isset($form['image'])) {
-            $path = $request->file('image')->store('public/image');
-            $profile->image_path = basename($path);
-        } else {
-            $news->image_path = null;
-        }
-        
-        unset($form['_token']);
-        unset($form['image']);
+        //課題１４
+        unset ($form['_token']);
         
         $profile->fill($form);
         $profile->save();
         
         return redirect('admin/profile/create');
     }
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+         $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
-    public function update()
+    public function update(Request $request)
     {
-        return redirect('admin/profile/edit');
+       $this->validate($request, Profile::$rules);
+        
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        //送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        
+        
+        unset($profile_form['_token']);
+       
+        $profile->fill($profile_form)->save(); 
+        
+        return redirect('admin/profile');
     }
 }
